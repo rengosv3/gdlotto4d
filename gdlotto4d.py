9a0f950411781a61a6de51bd9bd9526de021f9c8
rengosv3 <56378951+rengosv3@users.noreply.github.com>
2025-07-25 14:48:23 +0800

    Add files via upload

--- gdlotto4d.py
+++ gdlotto4d.py
@@ -17,7 +17,6 @@ from wheelpick import (
 )
 from hit_frequency import show_hit_frequency_tab
 from last_hit import show_last_hit_tab
-from digit_rank import show_digit_rank_tab
 
 st.set_page_config(page_title="Breakcode4D Predictor", layout="wide")
 st.title("🔮 Breakcode4D Predictor (GD Lotto) V2.0")
@@ -57,7 +56,7 @@ tabs = st.tabs([
     "Wheelpick",
     "Hit Frequency",
     "Last Hit",
-    "Digit Rank"
+    "Semak Fail"
 ])
 
 # Tab 1: Insight
@@ -166,7 +165,6 @@ with tabs[4]:
     likes = user_like.split()
     dislikes = user_dislike.split()
 
-    # Manual vs Auto toggle
     input_mode = st.radio("Input Base:", ["Auto dari strategi", "Manual"], key="wp_mode")
 
     if input_mode == "Auto dari strategi":
@@ -224,6 +222,14 @@ with tabs[5]:
 # Tab 7: Last Hit
 with tabs[6]:
     show_last_hit_tab(draws)
-    
-with tabs[7]:  # atau index sesuai
-    show_digit_rank_tab(draws)
+
+# Tab 8: Semak Fail
+with tabs[7]:
+    st.header("📁 Semak Kandungan draws.txt")
+    try:
+        with open("data/digit_rank.txt", "r", encoding="utf-8") as f:
+            content = f.read()
+            st.code(content, language='text')
+            st.download_button("📥 Muat Turun draws.txt", content, file_name="draws.txt")
+    except FileNotFoundError:
+        st.error("❌ `data/draws.txt` tidak wujud.")
